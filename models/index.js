077a21142944083f410b4a410cc3c20bd1bbcce8
naman <naman.v@hashtechy.com>
2023-10-06 13:22:08 +0530

    Establish the connection with database

---
+++ models/index.js
@@ -0,0 +1,54 @@
+const Sequelize = require("sequelize");
+require("dotenv").config();
+
+const sequelizeInstance = new Sequelize({
+    database: process.env.DB_NAME,
+    username: 'root',
+    password: process.env.DB_PASSWORD,
+    host: 'localhost',
+    dialect: 'mysql',
+});
+
+
+// here test the connection is estabilish successfully or not.
+(async () => {
+    try {
+        await sequelizeInstance.authenticate();
+        console.log('Connection has been established successfully.');
+      } catch (error) {
+        console.error('Unable to connect to the database:', error);
+    }
+})();
+
+module.exports = sequelizeInstance;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- server.js
+++ server.js
@@ -1,10 +1,20 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const db = require('./models');
 require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT || 3000;
+
+(async () => {
+    try {
+      await db.sync();
+      console.log('Database synchronized successfully.');
+    } catch (error) {
+      console.error('Error:', error);
+    }
+})();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -12,6 +22,9 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
+
+
+
 
 
 
